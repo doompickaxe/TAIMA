7dcc354139501f42e504363cc870620c78d66a48
Kevin Watzal <kevin.watzal@software-craftsmen.at>
2019-08-19 18:14:26 +0200

    Ignore unkown json properties

--- src/main/kotlin/io/kay/web/dto/CreateWorkPartDTO.kt
+++ src/main/kotlin/io/kay/web/dto/CreateWorkPartDTO.kt
@@ -1,10 +1,12 @@
 package io.kay.web.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.joda.time.LocalDate
 import org.joda.time.LocalTime
 import java.util.*
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class CreateWorkPartDTO(
     @JsonProperty("start") val start: LocalTime,
     @JsonProperty("end") val end: LocalTime? = null
