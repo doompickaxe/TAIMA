4d719bdc15735addccc48ed62a2c4780a7c0cd1c
Kevin Watzal <kevin.watzal@software-craftsmen.at>
2019-08-25 15:46:43 +0200

    Add more tests

--- src/test/kotlin/io/kay/service/LogRepositoryTest.kt
+++ src/test/kotlin/io/kay/service/LogRepositoryTest.kt
@@ -1,14 +1,19 @@
 package io.kay.service
 
 import io.kay.model.*
+import io.kay.web.dto.CreateWorkPartDTO
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.joda.time.DateTime
+import org.joda.time.LocalTime
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import kotlin.math.exp
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 class LogRepositoryTest {
 
@@ -81,5 +86,79 @@ class LogRepositoryTest {
         val result = repository.findWorkPartsByDay(DateTime.now(), "me@myself.test")
 
         assertEquals(listOf(expected.toWorkPartDTO()), result)
+    }
+
+    @Test
+    fun returnsNoWorkPartsOnOutrangedDate() {
+        val expected = transaction {
+            val me = User.new {
+                email = "me@myself.test"
+                role = UserRole.USER
+            }
+            WorkPart.new {
+                day = DateTime()
+                start = DateTime()
+                end = DateTime()
+                user = me
+            }
+        }
+
+        val result = repository.findWorkPartsByDay(expected.start.minusDays(1), expected.end!!.minusDays(1), "me@myself.test")
+
+        assertEquals(listOf(), result)
+    }
+
+    @Test
+    fun containsResultIfInDateRange() {
+        val expected = transaction {
+            val me = User.new {
+                email = "me@myself.test"
+                role = UserRole.USER
+            }
+            WorkPart.new {
+                day = DateTime()
+                start = DateTime()
+                end = DateTime()
+                user = me
+            }
+        }
+
+        val result = repository.findWorkPartsByDay(expected.start.minusDays(1), expected.end!!.plusDays(1), "me@myself.test")
+
+        assertEquals(listOf(expected.toWorkPartDTO()), result)
+    }
+
+    @Test
+    fun doesNotLogWorkIfFreepartIsLogged() {
+        transaction {
+            val me = User.new {
+                email = "me@myself.test"
+                role = UserRole.USER
+            }
+            FreePart.new {
+                day = DateTime()
+                user = me
+                reason = FreeType.ILL
+            }
+        }
+
+        val result = repository.logWork("me@myself.test", DateTime(), CreateWorkPartDTO(LocalTime()))
+
+        assertNull(result)
+    }
+
+    @Test
+    fun savesWork() {
+        val me = transaction {
+            User.new {
+                email = "me@myself.test"
+                role = UserRole.USER
+            }
+        }
+
+        val result = repository.logWork("me@myself.test", DateTime(), CreateWorkPartDTO(LocalTime()))
+
+        assertNotNull(result)
+        assertNotNull(result.id)
     }
 }
